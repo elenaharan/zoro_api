6144eca872556ef377946e42979bbfca1e0685f3
Elena <elenaharan@gmail.com>
2022-04-18 17:28:53 +0100

    basic node.js app set up

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+  uui = require('uuid'),
+  bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Welcome!!');
+});
+
+//Error handling
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Something went wrong!');
+});
+
+//Server
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
